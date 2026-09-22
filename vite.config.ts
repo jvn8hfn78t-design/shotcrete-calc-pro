4d77c49de882984039a662e6ab1cef4d67803e8d
jvn8hfn78t-design <jvn8hfn78t@privaterelay.appleid.com>
2026-09-22 14:50:45 -0500

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    spa: {},
+    spa: {
+      enabled: true,
+      prerender: {
+        outputPath: "/index.html",
+      },
+    },
   },
 });
