bb9beaed66a48e999672dd85a548bbeb0f0de3d7
jvn8hfn78t-design <jvn8hfn78t@privaterelay.appleid.com>
2026-09-22 16:00:55 -0500

    Actualizar capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.pdr.shotcrete",
-  appName: "PDR Shotcrete",
+  appName: "Shotcrete Calc Batch",
   webDir: ".output/public",
 };
 
