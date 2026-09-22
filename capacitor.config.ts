75ee23c09ddc034c83fc8699685c3b58f61c3272
jvn8hfn78t-design <jvn8hfn78t@privaterelay.appleid.com>
2026-09-22 14:23:12 -0500

    Crear capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.pdr.shotcrete",
+  appName: "PDR Shotcrete",
+  webDir: "dist/client",
+};
+
+export default config;
